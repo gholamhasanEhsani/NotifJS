940e3300070db42ef05d7f33c056a42c7ab1dd41
غلام حسن احسانی <gehsani@proton.me>
2024-06-07 11:45:49 +0330

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+const te = document.querySelector('#te');
+const co = document.querySelector('#co');
+const ti = document.querySelector('#ti');
+function fire() {
+    let t = te.value;
+    let c = co.value;
+    let i = ti.value;
+    Notif(t, c, i);
+}
