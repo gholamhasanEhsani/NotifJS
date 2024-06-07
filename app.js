168153ef7f6d87724f67b909f6a239663fda5a5f
غلام حسن احسانی <gehsani@proton.me>
2024-06-07 12:15:14 +0330

    update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-document.onload = () => {
-    console.log("X");
-    const te = document.querySelector('#te');
-    const co = document.querySelector('#co');
-    const ti = document.querySelector('#ti');
-    function fire() {
-        let t = te.value;
-        let c = co.value;
-        let i = ti.value;
-        Notif(t, c, i);
+function fire() {
+    document.onload = () => {
+        console.log("X");
+        const te = document.querySelector('#te');
+        const co = document.querySelector('#co');
+        const ti = document.querySelector('#ti');
+            let t = te.value;
+            let c = co.value;
+            let i = ti.value;
+            Notif(t, c, i);
     }
 }
